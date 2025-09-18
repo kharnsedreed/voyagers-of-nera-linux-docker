9ac7a1491dceaf05da3e1e69315249491a0984b8
Kharn Se'Dreed <33072826+kharnsedreed@users.noreply.github.com>
2025-09-18 21:48:18 +1000

    changing to just always perform the steamcmd to force update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,12 @@
-# Check if directory exists then perform initial creation
-if [ ! -d "/dedicated-server/BoatGame/Binaries/Win64" ]; then
-    steamcmd \
-    +@sSteamCmdForcePlatformType windows \
-    +force_install_dir /dedicated-server \
-    +login anonymous \
-    +app_update 3937860 validate \
-    +quit
-fi
+# Run the steam command and update always
+steamcmd \
++@sSteamCmdForcePlatformType windows \
++force_install_dir /dedicated-server \
++login anonymous \
++app_update 3937860 validate \
++quit
 
+# Change working directory temporarily and use wine to run the server.exe
 pushd /dedicated-server/BoatGame/Binaries/Win64 > /dev/null
 wine BoatGameServer-Win64-Shipping.exe
 popd > /dev/null
